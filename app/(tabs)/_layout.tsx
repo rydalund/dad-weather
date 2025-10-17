4d50f298c34e5a6b376ac0431f5a35a41b4f53c4
rydalund <54313809+rydalund@users.noreply.github.com>
2025-10-17 15:23:16 +0200

    style(index): apply basic layout and styling

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,19 +1,8 @@
 import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
-
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
-
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -21,37 +10,21 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
+        tabBarInactiveTintColor: '#999',
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
+          tabBarIcon: ({ color }) => <Ionicons name="home" size={24} color={color}  />,
         }}
       />
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <Ionicons name="partly-sunny" size={24} color={color} />,
         }}
       />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,44 @@
-import { StyleSheet } from 'react-native';
+import { StatusBar, StyleSheet, Image } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { Text, View } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+
+const BACKGROUND_COLOR = '#1e1e1e';
 
 export default function TabOneScreen() {
- return (
+  return (
+<SafeAreaView style={styles.safeArea} edges={['bottom']}>
     <View style={styles.container}>
+      <StatusBar backgroundColor={BACKGROUND_COLOR} barStyle="light-content" />
       <Text style={styles.title}>DadWeather</Text>
       <Text style={styles.tagline}>Serious forecasts, seriously dad jokes.</Text>
+      <Image
+        source={require('@/assets/images/app.png')}
+        style={styles.appImage}
+      />
+      <Text style={[styles.gpsPresentTemperature, styles.sharedStyling]}>30
+        <Text style={[styles.gpsPresentTemperature, styles.sharedStyling]}>°C</Text>
+      </Text>
+      <Text style={[styles.gpsPresentLocation, styles.sharedStyling]}>📍 Växjö</Text>
+      <Text style={[styles.tagline, styles.dadJokeIndex]}>"I told my wife she should embrace her mistakes. She gave me a hug..."</Text>
+      <View style={styles.iconBar}>
+        <Ionicons name="heart-sharp" style={styles.favoriteIcon} />
+        <Ionicons name="search-sharp" style={styles.searchIcon} />
+      </View>
     </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+    safeArea: {
+    flex: 1,
+    backgroundColor: BACKGROUND_COLOR,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#1e1e1e',
     alignItems: 'center',
     justifyContent: 'center',
     paddingHorizontal: 20,
@@ -23,7 +47,6 @@ const styles = StyleSheet.create({
     fontSize: 36,
     fontWeight: 'bold',
     color: '#ff6f00',
-    textTransform: 'uppercase',
     textShadowColor: '#000',
     textShadowOffset: { width: 2, height: 2 },
     textShadowRadius: 3,
@@ -39,4 +62,53 @@ const styles = StyleSheet.create({
     textShadowOffset: { width: 1, height: 1 },
     textShadowRadius: 2,
   },
+  appImage: {
+    width: 200,
+    height: 300,
+    resizeMode: 'contain',
+    marginTop: 20,
+    borderRadius: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+  },
+  sharedStyling: {
+    marginTop: 10,
+    fontWeight: 'bold',
+    textShadowColor: '#000',
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 3,
+  },
+  gpsPresentTemperature: {
+    color: '#ffffff',
+    fontSize: 36,
+    fontWeight: 'bold'
+  },
+  gpsPresentLocation: {
+    fontSize: 24,
+    color: 'gray',
+    marginBottom: 40,
+  },
+  iconBar: {
+    position: 'absolute',
+    bottom: 20,
+    left: 40,
+    right: 40,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  searchIcon: {
+    fontSize: 36,
+    color: '#ffffff',
+  },
+  favoriteIcon: {
+    fontSize: 36,
+    color: '#ff0000',
+  },
+  dadJokeIndex: {
+    marginTop: 20,
+    marginBottom: 50,
+  }
 });
